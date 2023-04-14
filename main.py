d4c72ff90d286cdcbae15360a724803732ff815a
Krystian Dutka <89588699+xkrystiandutka@users.noreply.github.com>
2023-04-14 20:37:01 +0200

    Final version of the software using the prototype

--- main.py
+++ main.py
@@ -11,11 +11,22 @@ class PlotGeneratorPrototype:
         self.current_plot = None
 
     def choose_file(self):
+        """
+            Choose a file using a file dialog. Only text files with the .txt extension are allowed.
+            Sets the 'file_path' attribute to the chosen file path.
+        """
         root = tk.Tk()
         root.withdraw()
         self.file_path = filedialog.askopenfilename(filetypes=[('Text Files', '*.txt')])
 
     def plot_line(self, x_label='', y_label='', title=''):
+        """
+            Plot a line chart using data from the input file.
+            :param x_label: label for the x-axis
+            :param y_label: label for the y-axis
+            :param title: title of the chart
+            :return: False if the input file is not selected or the data is invalid, True otherwise
+        """
         if not self.file_path:
             print('Error: select file')
             return False
@@ -35,6 +46,13 @@ class PlotGeneratorPrototype:
             self.current_plot = plt.gcf()
 
     def plot_bar(self, x_label='', y_label='', title=''):
+        """
+            Plot a bar chart using data from the input file.
+            :param x_label: label for the x-axis
+            :param y_label: label for the y-axis
+            :param title: title of the chart
+            :return: False if the input file is not selected or the data is invalid, True otherwise
+        """
         if not self.file_path:
             print('Error: select file')
             return False
@@ -56,6 +74,11 @@ class PlotGeneratorPrototype:
             self.current_plot = fig
 
     def plot_pie(self, title=''):
+        """
+            Plot a pie chart using data from the input file.
+            :param title: title of the chart
+            :return: False if the input file is not selected or the data is invalid, True otherwise
+        """
         if not self.file_path:
             print('Error: select file')
             return False
@@ -67,9 +90,17 @@ class PlotGeneratorPrototype:
             values = [float(d.split(',')[1]) for d in data]
             plt.pie(values, labels=labels)
             plt.title(title)
+            self.current_plot = plt.gcf()
 
 
     def _validate_data(self, data):
+        """
+        Validates the input data for a plotting function. It checks if the data contains at least two lines, if each line contains
+        at least one comma-separated value, and if all values can be converted to floats. Prints error messages and returns False
+        if any of these conditions are not met. Returns True otherwise.
+        :param data: a list of strings representing the input data
+        :return: True if the input data is valid, False otherwise
+        """
         if len(data) < 2:
             print('Error: input file should contain at least 2 lines')
             return False
@@ -99,6 +130,12 @@ class PlotGeneratorPrototype:
         return True
 
     def save_current_plot(self, title):
+        """
+            Save the current plot to a file with the given title.
+            If the current plot is None, print an error message and return.
+            The plot is saved in a subdirectory 'plots' with the format 'title.png'.
+            :param title: title of the saved file
+        """
         if self.current_plot is None:
             print('Error: no chart to save')
             return
@@ -109,24 +146,32 @@ class PlotGeneratorPrototype:
         print(f'The chart was saved as a file {file_path}')
 
     def clone(self):
+        """
+            Create a deep copy of the object.
+            :return: a new instance of the PlotGeneratorPrototype class with the same attributes
+        """
         return copy.deepcopy(self)
 
-# Tworzymy nowy obiekt klasy PlotGeneratorPrototype
-plot_generator = PlotGeneratorPrototype()
-plot_generator.choose_file()
+def main():
+    """
+    The main function demonstrates the prototype design pattern using a PlotGeneratorPrototype class. It creates an instance
+    of the class, allows the user to select a data file, clones the object to create new objects for line plot, pie chart,
+    and bar plot generation, customizes each plot with specific parameters, and saves each plot with a corresponding file name.
+    """
+    plot_generator = PlotGeneratorPrototype()
+    plot_generator.choose_file()
+    line_plot_generator = plot_generator.clone()
+    line_plot_generator.plot_line(x_label='X Label', y_label='Y Label', title='Line Plot')
+    line_plot_generator.save_current_plot('Line Plot')
 
-# Kopiujemy obiekt i zmieniamy parametry
-line_plot_generator = plot_generator.clone()
-line_plot_generator.plot_line(x_label='X Label', y_label='Y Label', title='Line Plot')
-line_plot_generator.save_current_plot('Line Plot')
+    pie_plot_generator = plot_generator.clone() # Copy the object and change the parameters
+    pie_plot_generator.plot_pie(title='Pie Chart')
+    pie_plot_generator.save_current_plot('Pie Chart')
 
-# Kopiujemy obiekt i zmieniamy parametry
-bar_plot_generator = plot_generator.clone()
-bar_plot_generator.plot_bar(x_label='X Label', y_label='Y Label', title='Bar Plot')
-bar_plot_generator.save_current_plot('Bar Plot')
+    bar_plot_generator = plot_generator.clone() # Copy the object and change the parameters
+    bar_plot_generator.plot_bar(x_label='X Label', y_label='Y Label', title='Bar Plot')
+    bar_plot_generator.save_current_plot('Bar Plot')
 
-# Kopiujemy obiekt i zmieniamy parametry
-pie_plot_generator = plot_generator.clone()
-pie_plot_generator.plot_pie(title='Pie Chart')
-pie_plot_generator.save_current_plot('Pie Chart')
 
+if __name__ == '__main__':
+    main()
